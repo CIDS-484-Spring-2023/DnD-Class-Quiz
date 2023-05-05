56029d373cbbc18876daac9ecd81a223a6870678
JacksonGergen <123509223+JacksonGergen@users.noreply.github.com>
2023-05-05 15:42:09 -0500

    Update Senior Capstone - Final Project.cpp

--- Senior Capstone - Final Project.cpp
+++ Senior Capstone - Final Project.cpp
@@ -3,22 +3,6 @@
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-
-/*
-	Name: Rough Draft
-	Author: Jackson Gergen
-	Date: 01/02/23 14:07
-	Description: This is a rough draft of what the code will look like. Not fully set on the point system yet,
-					but this is a start!
-*/
-
 #include <iostream>
 #include <string>
 #include <thread>
